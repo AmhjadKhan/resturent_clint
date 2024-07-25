22fbb2f9743e465d452d0704abc0982fed5e73e2
Amhjadkhan <amjadpogramer@gmail.com>
2024-07-26 02:57:12 +0400

    this is no header-footer in login page commiit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,27 @@
 import { Helmet } from "react-helmet-async"
 
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
   return (
     <div>
       <>
-            <Helmet>
+          <Helmet>
                 <title>Bistro Boss | Login</title>
             </Helmet>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero min-h-screen bg-blue-300">
                 <div className="hero-content flex-col md:flex-row-reverse">
                     <div className="text-center md:w-1/2 lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="text-5xl font-bold ml-7 mb-6">Login now!</h1>
+                       <img className="h-72 w-96" src="https://res.cloudinary.com/tf-lab/image/upload/restaurant/ccb4f650-e783-43f0-981c-d41a9a0e6898/7c069396-9de8-43e3-a849-f7d17ccfe3ac.jpg" alt="" />
                     </div>
                     <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-                        <form  className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
